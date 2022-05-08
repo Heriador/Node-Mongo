f83a2896ac4ec8ff00043d0a612938c4a473917e
Angel Miranda <mirandaangel83@gmail.com>
2022-05-07 22:01:07 -0500

    handlebars fixed

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app = express ();
 // Setting
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname,'views'));
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
